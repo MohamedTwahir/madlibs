145e0c8153cd3b3c26517f8d96977516e999f29a
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-06-22 17:51:55 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Am learning python through projects :
+Today i will be building a madlibs game 
+Madlib is a phrasal template word game where one player prompts others for a list of word to substitute for blank in a story.
---
+++ madlib.py
@@ -0,0 +1,16 @@
+# Prompting a player to enter a name:
+name =  input('What is yoyr name?: ')
+# asking for an adjective from a user
+adjective = input('Enter an adjective: ')
+#prompting a user for a plural noun
+noun_1 = input('Enter a plural noun: ')
+
+# Prompting a user to enter another name/noun
+noun_2 = input('Enter any other noun: ')
+
+
+
+#printing a ryhme
+print("Roses are " + adjective)
+print(noun_1  + "are blue")
+print("I can't start my day without", noun_2)
